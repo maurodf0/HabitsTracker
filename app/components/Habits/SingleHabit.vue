525970c6e42d9785d3d62f40680edd0bce36535a
Mauro <maurodefalco96@gmail.com>
2026-01-12 23:07:22 +0100

    Ristruttura il componente SingleHabit per gestire le abitudini con titolo, data e stato di completamento

--- app/components/Habits/SingleHabit.vue
+++ app/components/Habits/SingleHabit.vue
@@ -1,20 +1,22 @@
 <script setup lang="ts">
 
 const props = defineProps<{
-  title: string
+  title: string,
+  date: number,
+  todayCompleted: boolean
 }>()
 
 </script>
 
 <template>
-<UCard class="my-4" variant="subtle">
-    <template #header>
-          {{ title }}
-    </template>
-
-    <template #footer>
-     <small> 8 days Streak </small>
-    </template>
+<UCard  
+  variant="subtle" 
+  class="my-4 flex">
+<input type="checkbox" v-model="props.todayCompleted">
+<div class="flex flex-col">
+<h4 class="font-medium">{{ title }}</h4>
+<div class="flex"><small v-if="date > 5" class="text-[10px]">🔥</small> <small>  {{ date }} days Streak </small></div>
+</div>
   </UCard>
 </template>
 
--- app/pages/habits.vue
+++ app/pages/habits.vue
@@ -2,16 +2,39 @@
 import SingleHabit from '~/components/Habits/SingleHabit.vue';
 
 const Habits = [
-  'Learn Vue',
-  'Learn Nuxt',
-  'Go to Gym',
-  'Pet Kira'
-]
+  {
+  title: 'Learn Vue',
+  date: 2,
+  todayCompleted: false
+},
+  {
+  title: 'Go to Gym everyday',
+  date: 11,
+  todayCompleted: false
+},
+  {
+  title: 'Learn Nuxt',
+  date: 9,
+  todayCompleted: false
+},
+  {
+  title: 'Eat Healty',
+  date: 4,
+  todayCompleted: false
+},
+
+
+
+] 
 
 </script>
 
 <template>
-  <h1 class="text-4xl mt-4">Habits</h1>
-
-  <SingleHabit v-for="(Habit, index) in Habits" :title="Habit" />
+  <div class="flex justify-between items-center mt-4">
+    <h1 class="text-2xl font-medium">Good Morning, User</h1>
+    <div class="text-sm total"> 0 / {{ Habits.length }}</div>
+  </div>
+  
+  <h3 class="textx-xl mt-4">Today's Habit</h3>
+  <SingleHabit v-for="(Habit, index) in Habits" :title="Habit.title" :date="Habit.date" />
 </template>
