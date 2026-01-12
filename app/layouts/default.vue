0c91f4edfc4051762ba8cccb1dcc1dad4cf8ba43
Mauro <maurodefalco96@gmail.com>
2026-01-12 13:22:03 +0100

    Init

---
+++ app/layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+    <Header>
+        Habits Tracker
+    </Header>
+    <slot></slot>
+    <Footer>
+        <div class="flex justify-center">
+            <a href="https://nuxtjs.org" target="_blank" class="button--green">Documentation</a>
+            <a href="https://github.com/nuxt/nuxt" target="_blank" class="button--grey">GitHub</a>
+        </div>
+    </Footer>
+</template>
